30f39b3bc1cd9391fb8f8c3cd5d97362fa020531
Moopy559 <125260487+Moopy559@users.noreply.github.com>
2023-12-22 10:52:21 +1000

    Update table and form logic, add Delete button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let delCounter = 0;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,20 +12,49 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary("Lord of the Rings", "Sammo", "25", "No");
-addBookToLibrary("Poo Bum", "Mumen", "46", "No");
-addBookToLibrary("Captain Underpants", "Matthew", "97", "Yes");
-
+// Populates the table and adds 'Delete' Button
 function displayBook() {
   const libTable = document.querySelector("#lib-table");
+  let lastIndex = myLibrary.length - 1;
+  let lastEntry = myLibrary[lastIndex];
 
-  for (let i = 0; i < myLibrary.length; i++) {
-    const dataRow = libTable.insertRow();
-    for (const key in myLibrary[i]) {
-      const td = dataRow.insertCell();
-      td.textContent = myLibrary[i][key];
-    }
+  const dataRow = libTable.insertRow();
+  for (const key in lastEntry) {
+    const td = dataRow.insertCell();
+    td.textContent = lastEntry[key];
   }
+  delCounter++;
+  const delCell = dataRow.insertCell();
+  const delButton = document.createElement("button");
+  delButton.setAttribute("id", delCounter);
+  delButton.textContent = "X";
+  delCell.appendChild(delButton);
 }
 
-displayBook();
+// Resets the form to its initial state
+function resetForm() {
+  const inputs = document.querySelectorAll("input");
+
+  inputs.forEach((input) => {
+    input.value = "";
+    input.checked = false;
+  });
+}
+
+// Submits the form data to be populated
+function submitForm() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  let readValue = "";
+  const readYes = document.getElementById("read-yes");
+  if (readYes.checked) {
+    readValue = "Yes";
+  } else {
+    readValue = "No";
+  }
+
+  addBookToLibrary(title, author, pages, readValue);
+  displayBook();
+  resetForm();
+}
